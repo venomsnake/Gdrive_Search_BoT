a19d49e7bf5695d71a3a420723ace568bf16121a
venomsnake <thakurmark07@gmail.com>
2022-02-01 15:05:41 +0530

    Init

---
+++ start.sh
@@ -0,0 +1,7 @@
+python3 -m bot
+
+git clone https://venomsnake/Gdrive_Search_BoT /root/searchbot
+cp CREDS/.env /root/searchbot/.env
+cd /root/searchbot
+pip3 install -r requirements.txt
+python3 -m bot
