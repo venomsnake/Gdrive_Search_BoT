22137a127960492cdb077431a36d9fd5816e8ad3
Venom Snake <78187975+venomsnake@users.noreply.github.com>
2022-02-01 15:26:09 +0530

    CLEANED

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
 python3 -m bot
-
-git clone https://venomsnake/Gdrive_Search_BoT /root/searchbot
-cp CREDS/.env /root/searchbot/.env
-cd /root/searchbot
-pip3 install -r requirements.txt
-python3 -m bot
