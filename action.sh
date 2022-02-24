4cd0b4818ec60d5b6566cf8cea62dcce458f6701
Venom Snake <78187975+venomsnake@users.noreply.github.com>
2022-02-24 16:04:18 +0530

    Test

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-git clone https://venomsnake/Gdrive_Search_BoT /root/searchbot
+git clone https://venomsnake/Gdrive_Search_BoT searchbot
 cd searchbot
-cp CREDS/config_sample.env /root/searchbot/config.env
+cp CREDS/config_sample.env config.env
 pip3 install -r requirements.txt
 sudo dockerd
 sudo docker build . -t searchbot
