4d58e6eb71bc33debabbbc35a02733016f03fc30
venomsnake <thakurmark07@gmail.com>
2022-02-24 15:51:31 +0530

    Fix

--- action.sh
+++ action.sh
@@ -1,6 +1,7 @@
 git clone https://venomsnake/Gdrive_Search_BoT /root/searchbot
-cp CREDS/.env /root/searchbot/.env
-cd /root/searchbot
+cd searchbot
+cp CREDS/config_sample.env /root/searchbot/config.env
+pip3 install -r requirements.txt
 sudo dockerd
 sudo docker build . -t searchbot
 sudo docker run searchbot
